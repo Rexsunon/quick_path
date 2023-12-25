c1505bd9fb5fc650a909167bcb1fd044f3c96687
Rexsunon <rexsunon.dev@gmail.com>
2023-12-25 23:38:53 +0100

    add: install required dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quick_path/src/quick_path.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const QuickPath());
+  runApp(const ProviderScope(child: QuickPath()));
 }
